431b89f21f027a6180d9ac91c44a2f1aab48daf2
jeffrey Soriano <jsoriano5>
2014-07-16 23:01:32 -0400

    Added filtered action

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,13 @@
+require 'api'
+
 class HomeController < ApplicationController
 
 	def welcome
 	end
 
 	def search
+		@card_api = HeartApi.new
+		@filtered = @card_api.get_cards_by_cost(0, params[:cost].to_i)
 	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
 
   get '/welcome' => 'home#welcome'
   get '/search' => 'home#search'
+  get '/search/:cost' => 'home#search'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
