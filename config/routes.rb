efdaa086759f8f4280881de0ce6a801a9586ec5a
Sebastian Lugo <sebalugo@ubuntu.(none)>
2014-07-16 20:58:45 -0400

    Home controller,welcome and search action

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+
+  get '/welcome' => 'home#welcome'
+  get '/search' => 'home#search'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
