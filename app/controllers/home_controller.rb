ccd994a215c1fd8330a39b41d12beee40c136b03
Nelson Taveras <ntaveras03@gmail.com>
2014-07-16 23:06:20 -0400

    Fixed some typos in the search form

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,6 +6,7 @@ class HomeController < ApplicationController
 	end
 
 	def search
+		# render :text => params.inspect
 		@card_api = HeartApi.new
 		@filtered = @card_api.get_cards_by_cost(0, params[:cost].to_i)
 	end
