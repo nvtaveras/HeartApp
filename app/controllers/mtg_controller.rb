a0197d1826ea81d6a3c70fceab51acac029ebedd
jeffrey Soriano <jsoriano5>
2014-07-22 08:31:51 -0400

    Changed key for mtg costs and added logic to search view

--- app/controllers/mtg_controller.rb
+++ app/controllers/mtg_controller.rb
@@ -11,7 +11,14 @@ class MtgController < ApplicationController
 	end
 
 	def resultsmtg
+		@mtg_api = MTGApi.new
+		@filtered = []
+		
+		@filtered = @mtg_api.can_buy_from_set(params[:set], params[:white_cost], params[:blue_cost], params[:black_cost], params[:red_cost], params[:green_cost])
 
+		flash.now[:notice] = "Your search returned #{@filtered.length} results!"
+
+		@filtered = @filtered.paginate(:page => params[:page],:per_page => 10)
 	end
 
 end
