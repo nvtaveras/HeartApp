3fb190860c8562d6313db06c506d3964996f238c
jeffrey Soriano <jsoriano5>
2014-07-22 01:43:20 -0400

    Changed card api to global vvariable to add efficiency to code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,6 +3,8 @@ require 'api'
 
 class HomeController < ApplicationController
 
+	$card_api = HeartApi.new
+	
 	def welcome
 	end
 
@@ -11,17 +13,15 @@ class HomeController < ApplicationController
 	end
 
 	def cardinfo
-		@card_api = HeartApi.new
-		@card = @card_api.get_cards.find{ |x| x.id_api == params[:id_api].to_i}
+		@card = $card_api.get_cards.find{ |x| x.id_api == params[:id_api].to_i}
 	end
 
 	def results
-		@card_api = HeartApi.new
 		@filtered = []
 		@filtered2 = []
 		if params[:cost].present?
-			@filtered = @card_api.get_cards_by_cost(0, params[:cost].to_i)
-			@filtered2 = @card_api.get_cards_by_cost_optimized(params[:cost].to_i)
+			@filtered = $card_api.get_cards_by_cost(0, params[:cost].to_i)
+			@filtered2 = $card_api.get_cards_by_cost_optimized(params[:cost].to_i)
 			flash.now[:notice] = "Your search returned #{@filtered2.length} results!"
 		end
 		@filtered2 = @filtered2.paginate(:page => params[:page],:per_page => 10)
