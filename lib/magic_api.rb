fc95060d1457fa9ab5fdf97f7a49770a38ea2220
Nelson Taveras <ntaveras03@gmail.com>
2014-07-22 07:15:52 -0400

    Added can_buy method to the magic_api lib

--- lib/magic_api.rb
+++ lib/magic_api.rb
@@ -22,4 +22,39 @@ class HeartApi
 		cards
 	end
 
+	def can_buy card, w, u, b, r, g
+		# How much do i got of each type
+		got = Hash.new
+		got['W'] = w
+		got['U'] = u
+		got['B'] = b
+		got['R'] = r
+		got['G'] = g
+		manacost = card.manacost
+		idx = 0
+		extra = 0 # Required extra cards                                                                                                                                                                                                                                           cxx
+		while idx < manacost.length do
+			if is_i?(manacost[idx])
+				extra = extra * 10
+				extra = extra + manacost[idx].to_i
+			else
+				got[manacost[idx]] = got[manacost[idx]] - 1;
+			end
+			idx = idx + 1
+		end
+		f = 1 # set to 1 because right now i can buy the card
+		got.each do |k, v|
+			if v < 0
+				f = 0 # takes more than what i have, can't buy.
+			else
+				extra = extra - v
+			end
+		end
+		f && extra <= 0
+	end
+
+	def is_i? c
+       c >= '0' && c <= '9'
+    end
+
 end
